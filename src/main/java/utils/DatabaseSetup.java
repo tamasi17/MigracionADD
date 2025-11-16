9abac3c04da1c2587e16e743f86290b08040f7a4
mats <mat.eidelman@gmail.com>
2025-11-16 15:49:16 +0100

    (DbSetup) borrarTabla(tabla) logic defined

--- src/main/java/utils/DatabaseSetup.java
+++ src/main/java/utils/DatabaseSetup.java
@@ -1,29 +1,53 @@
 package main.java.utils;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Set;
 
 import static main.java.logging.LoggerProvider.getLogger;
 
 public class DatabaseSetup {
 
 
-    void borrarTablas(){
-        String sql = "DROP";
+    public static void borrarTabla(String tabla){
+
+        Set<String> tablasPermitidas = Set.of("clientes","pedidos", "productos");
+
+        // si la tabla que pasamos por parametro no se encuentra
+        // entre las tablas permitidas para borrado, tira excepcion
+        if (!tablasPermitidas.contains(tabla)){
+            throw new IllegalArgumentException(tabla + "no es un argumento valido.");
+        }
+
+        String sql = "DROP TABLE "+ tabla+ ";";
+
+        try (Connection connection = ConnectionFactory.getConnectionDmOriginal();
+             Statement statement = connection.createStatement();){
+
+            statement.executeUpdate(sql);
+            getLogger().info("Tabla "+ tabla +" eliminada correctamente.");
+
+        } catch (SQLException sqle) {
+            getLogger().error("No se pudo borrar la tabla "+ tabla);
+            System.err.println(sqle.getLocalizedMessage());
+
+        }
+
     }
 
     public static void crearTablaClientes() throws SQLException {
 
         String sql = """
                 CREATE TABLE IF NOT EXISTS clientes (
-                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    idCliente INT AUTO_INCREMENT PRIMARY KEY,
                     nombre VARCHAR(100) NOT NULL,
                     apellido1 VARCHAR(100) NOT NULL,
                     apellido2 VARCHAR(100) NOT NULL,
                     dni INT NOT NULL UNIQUE,
                     telefono BIGINT,
-                    activo BOOLEAN NOT NULL,
+                    activo BOOLEAN DEFAULT TRUE,
                     fecha_registro DATE
                 )
                 """;
@@ -46,7 +70,7 @@ public class DatabaseSetup {
 
         String sql = """
                 CREATE TABLE IF NOT EXISTS pedidos (
-                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    idCliente INT AUTO_INCREMENT PRIMARY KEY,
                     nombre VARCHAR(100) NOT NULL,
                     telefono BIGINT,
                     activo BOOLEAN NOT NULL,
